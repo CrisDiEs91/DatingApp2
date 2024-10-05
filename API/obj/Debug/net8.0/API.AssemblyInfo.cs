7a7f6c9c5cdbefa4f037701f61468fa17bd36729
Cristina Díaz Esquivel <cristinaesquivel959@gmail.com>
2024-10-05 00:48:35 -0600

    Parcial 01 - Add Data Context

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3abd6acbee6e4beedfcd83ba6eba4c060f978d0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bb471d651f7e8d0c8f4b22ff2806d0c68005dc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
