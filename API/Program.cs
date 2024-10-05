bbc9f9d01700545f4234672943ee8a2d0a09bf69
Cristina Díaz Esquivel <cristinaesquivel959@gmail.com>
2024-10-05 14:30:46 -0600

    Parcial01 - Add CORS

--- API/Program.cs
+++ API/Program.cs
@@ -10,15 +10,17 @@ builder.Services.AddDbContext<DataContext>(opt=>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
+//Configure the HTTP request pipeline
+app.UseCors((cors) => cors
+.AllowAnyHeader()
+.AllowAnyMethod()
+.WithOrigins(
+    "http:localhost:4200",
+    "https:localhost:4200"));
+
 
 app.MapControllers();
 
