efe4103f9dd4999e4c38b940b9156e06a6140f80
Cristina Díaz Esquivel <cristinaesquivel959@gmail.com>
2024-10-05 00:18:48 -0600

    .

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1807d31d43b0bd9e72f0a4adac770385b4c3385c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3abd6acbee6e4beedfcd83ba6eba4c060f978d0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
