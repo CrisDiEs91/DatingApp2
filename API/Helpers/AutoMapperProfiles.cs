e6b2d7a6174b2b0ac62a5fb783cb4456ba4e99d5
Cristina Díaz Esquivel <cristinaesquivel959@gmail.com>
2024-10-27 22:59:45 -0600

    Parcial02 - Configure AutoMapper

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -3,11 +3,14 @@ namespace API.Helpers;
 using API.DTOs;
 using API.Entities;
 using AutoMapper;
+
 public class AutoMapperProfiles : Profile
 {
     public AutoMapperProfiles()
     {
-        CreateMap<AppUser, MemberResponse>();
+        CreateMap<AppUser, MemberResponse>()
+            .ForMember(d => d.PhotoUrl,
+                o => o.MapFrom(s => s.Photos.FirstOrDefault(p => p.IsMain)!.Url));
         CreateMap<Photo, PhotoResponse>();
     }
 }
