93b57ae3e8aa2110a169f716088d9c92aa5ffd5b
Cristina Díaz Esquivel <cristinaesquivel959@gmail.com>
2024-12-01 18:06:32 -0600

    Parcial02 - Add AutoMapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,34 +1,30 @@
 namespace API.Controllers;
 using API.Data;
-using API.Data.Migrations;
-using API.DTOs;
 using API.DataEntities;
-using AutoMapper;
+using API.DTOs;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc;
 
 [Authorize]
 public class UsersController : BaseApiController
 {
     private readonly IUserRepository _repository;
-    private readonly IMapper _mapper;
 
     public UsersController(IUserRepository repository)
     {
         _repository = repository;
     }
 
-    //[AllowAnonymous]
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetAllAsync()
+    public async Task<ActionResult<IEnumerable<MemberResponse>>> GetAllAsync()
     {
         var users = await _repository.GetAllAsync();
+
         return Ok(users);
     }
 
-    [HttpGet("{username}")] // api/users/Calamardo
-    public async Task<ActionResult<AppUser>> GetByIdAsync(int id)
+    [HttpGet("{id:int}")] // api/users/2
+    public async Task<ActionResult<MemberResponse>> GetByIdAsync(int id)
     {
         var user = await _repository.GetByIdAsync(id);
 
@@ -41,13 +37,16 @@ public class UsersController : BaseApiController
     }
 
     [HttpGet("{username}")] // api/users/Calamardo
-    public async Task<ActionResult<AppUser>> GetByUsernameAsync(string username)
+    public async Task<ActionResult<MemberResponse>> GetByIdAsync(int id)
     {
-        var user = await _repository.GetByUsernameAsync(username);
+        var user = await _repository.GetByIdAsync(id);
+
         if (user == null)
         {
             return NotFound();
         }
+
         return user;
     }
-}
+
+    [HttpGet("{username}")] // api/users/Calamardo
